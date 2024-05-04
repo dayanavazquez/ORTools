ed0e11c57d8e9ee93d01bffa90a49c5083fbc214
dayi <dayivr18@gmail.com>
2024-05-04 00:24:51 -0400

    feat: add vehicle problem examples

---
+++ vrp/vrp_time_limit_problem.py
@@ -0,0 +1,104 @@
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+# [START solution_printer]
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    max_route_distance = 0
+    for vehicle_id in range(manager.GetNumberOfVehicles()):
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += f" {manager.IndexToNode(index)} -> "
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        plan_output += f"{manager.IndexToNode(index)}\n"
+        plan_output += f"Distance of the route: {route_distance}m\n"
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print(f"Maximum of the route distances: {max_route_distance}m")
+    # [END solution_printer]
+
+
+def main():
+    """Solve the cvrp problem."""
+    # Instantiate the data problem.
+    # [START data]
+    num_locations = 20
+    num_vehicles = 5
+    depot = 0
+    # [END data]
+
+    # Create the routing index manager.
+    # [START index_manager]
+    manager = pywrapcp.RoutingIndexManager(num_locations, num_vehicles, depot)
+    # [END index_manager]
+
+    # Create Routing Model.
+    # [START routing_model]
+    routing = pywrapcp.RoutingModel(manager)
+
+    # [END routing_model]
+
+    # Create and register a transit callback.
+    # [START transit_callback]
+    def distance_callback(from_index, to_index):
+        # pylint: disable=unused-argument
+        """Returns the distance between the two nodes."""
+        return 1
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    # [END transit_callback]
+
+    # Define cost of each arc.
+    # [START arc_cost]
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    # [END arc_cost]
+
+    # Add Distance constraint.
+    # [START distance_constraint]
+    dimension_name = "Distance"
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name,
+    )
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+    # [END distance_constraint]
+
+    # Setting first solution heuristic.
+    # [START parameters]
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+    search_parameters.log_search = True
+    search_parameters.time_limit.FromSeconds(5)
+    # [END parameters]
+
+    # Solve the problem.
+    # [START solve]
+    solution = routing.SolveWithParameters(search_parameters)
+    # [END solve]
+
+    # Print solution on console.
+    # [START print_solution]
+    if solution:
+        print_solution(manager, routing, solution)
+    # [END print_solution]
+
+
+if __name__ == "__main__":
+    main()
